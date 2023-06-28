3fd38d69fe9dae6262153f919bc10d524a69b14a
ScottCRed <ScottC@redpandasoftware.co.za>
2023-06-28 15:55:43 +0200

    Impliment InfoField Test into Personal component

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,29 +1,47 @@
 import Input from './Input.js';
 import Button from './Button';
 import DateInput from './DateInput';
+import InfoField from './InfoField'
+import { useState } from 'react';
 
 function Personal () {
-    
+    const [personalInfo, setInfo] = useState({
+        firstName: '',
+        surname: '',
+        title: '',
+        address: '',
+        phoneNo: '',
+        email: '',
+        date: '',
+    })
     const onClickOne = (e) => {
-        console.log('Add a thing bebe');
+        console.log(personalInfo);
         
       }
     
+      function handleInfoChange (e) {
+        const { id, value} = e.target;
+        setInfo((prevInfo) => ({
+            ...prevInfo,
+            [id] : value
+        }));
+      }
       
       const onClickTwo = (e) => {
         console.log(e.target.id);
       }
     return (
         <div>
-            <Input type='text' id='firstName' placeholder='First Name' />
-            <Input type='text' id='surname' placeholder='Surname' />
-            <Input type='text' id='title' placeholder='Title' />
-            <Input type='text' id='address' placeholder='Address' />
-            <Input type='text' id='phoneNo' placeholder='Phone Number' />
-            <Input type='text' id='email' placeholder='Email Address' />
-            <Input type='text' id='notes' placeholder='Other Notes' />
-            <DateInput type='date' id='firstDate' placeholder='First date'/>
+            <Input type='text' id='firstName' placeholder='First Name' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='surname' placeholder='Surname' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='title' placeholder='Title' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='address' placeholder='Address' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='phoneNo' placeholder='Phone Number' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='email' placeholder='Email Address' onInfoChange={handleInfoChange}/>
+            <Input type='text' id='notes' placeholder='Other Notes' onInfoChange={handleInfoChange}/>
+            <DateInput type='date' id='date' placeholder='First date' onDateChange={handleInfoChange}/>
             <Button onClick={onClickOne} buttonText='Retrieve Info' buttonClass='addButton' id='1'/>
+            <InfoField personalInfo={personalInfo}/>
         </div>
 
     )
