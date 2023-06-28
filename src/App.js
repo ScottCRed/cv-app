d867c55813f4a452aea118521cc2d3476886fdfc
ScottCRed <ScottC@redpandasoftware.co.za>
2023-06-28 11:48:00 +0200

    create input component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Input from './components/Input.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi :)</h1>
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Input () {
+    return (
+        <h2>it's me :)</h2>
+    )
+}
+
+export default Input
