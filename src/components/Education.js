0af56bb7110693f1ec67eebda53e25560fcbe3ce
ScottCRed <ScottC@redpandasoftware.co.za>
2023-06-29 11:24:04 +0200

    Insert necessary components into education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,7 +4,7 @@ import DateInput from './DateInput';
 import InfoField from './InfoField'
 
 const Education = (props) => {
-    const {education, setEducation} = props;
+    const {education, setEducation, eduInfo, setEduInfo} = props;
     
     const onClickOne = (e) => {
         console.log(education);
@@ -16,19 +16,22 @@ const Education = (props) => {
             ...prevInfo,
             [id] : value
         }));
+
+        const onAddEdu = (e) => {
+            e.preventDefault();
+
+            setEduInfo()
+        }
       }
     return (
         <div>
-            {/* <Input type='text' id='firstName' placeholder='First Name' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='surname' placeholder='Surname' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='title' placeholder='Title' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='address' placeholder='Address' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='phoneNo' placeholder='Phone Number' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='email' placeholder='Email Address' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='notes' placeholder='Other Notes' onInfoChange={handleInfoChange}/>
-            <DateInput type='date' id='date' placeholder='First date' onDateChange={handleInfoChange}/>
+            <Input type='text' id='institution' placeholder='Name of Institution' onInfoChange={handleEduChange}/>
+            <Input type='text' id='institutionAddress' placeholder='Address' onInfoChange={handleEduChange}/>
+            <Input type='text' id='degree' placeholder='Name of Qualification' onInfoChange={handleEduChange}/>
+            <Input type='text' id='majors' placeholder='Major/Specialization' onInfoChange={handleEduChange}/>
+            <DateInput type='date' id='startDate' placeholder='Start Date' onDateChange={handleEduChange}/>
+            <DateInput type='date' id='endDate' placeholder='Date of completion (current date if still in progress)' onDateChange={handleEduChange}/>
             <Button onClick={onClickOne} buttonText='Retrieve Info' buttonClass='addButton' id='1'/>
-            <InfoField personalInfo={personalInfo}/> */}
         </div>
 
     )
