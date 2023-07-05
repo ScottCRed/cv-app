ef95ce54696a5a1a7a3f89e069bd9b06eddf4f62
ScottCRed <ScottC@redpandasoftware.co.za>
2023-07-05 13:25:04 +0200

    Clean up

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,13 +1,7 @@
 import Input from './Input.js';
-import Button from './Button';
-import DateInput from './DateInput';
 
 const Personal = (props) => {
-    const {personalInfo, setInfo} = props;
-    
-    const onClickOne = (e) => {
-        console.log(personalInfo);
-      }
+    const { setInfo } = props;
     
       function handleInfoChange (e) {
         const { id, value} = e.target;
@@ -23,7 +17,7 @@ const Personal = (props) => {
             <Input type='text' id='firstName' placeholder='First Name' onInfoChange={handleInfoChange}/>
             <Input type='text' id='surname' placeholder='Surname' onInfoChange={handleInfoChange}/>
             <Input type='text' id='title' placeholder='Title' onInfoChange={handleInfoChange}/>
-            <Input type='text' id='age' placeholder='Age' onDateChange={handleInfoChange}/>
+            <Input type='text' id='age' placeholder='Age' onInfoChange={handleInfoChange}/>
             <Input type='text' id='address' placeholder='Address' onInfoChange={handleInfoChange}/>
             <Input type='text' id='phoneNo' placeholder='Phone Number' onInfoChange={handleInfoChange}/>
             <Input type='text' id='email' placeholder='Email Address' onInfoChange={handleInfoChange}/>
